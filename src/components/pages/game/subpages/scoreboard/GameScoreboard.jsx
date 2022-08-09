4fc756ee3b266c85e54e279003cdcf720ba46e3d
milesman34 <milesmanmusic@yahoo.com>
2022-08-09 15:07:54 -0400

    Scoreboard display depends on if game is being played

--- src/components/pages/game/subpages/scoreboard/GameScoreboard.jsx
+++ src/components/pages/game/subpages/scoreboard/GameScoreboard.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectGameState } from "../../../../../redux/selectors/game";
 import "./GameScoreboard.css"
+import "../../../../../common.css"
 
 export default () => {
+    const gameState = useSelector(selectGameState);
+
     return (
-        <div className="game-scoreboard">
-            Game Scoreboard
-        </div>
+        <div className="game-scoreboard-container">
+            {gameState === "start" ? 
+            <div className="game-scoreboard-placeholder flex-center-column">
+                No game being played!
+            </div> :
+            
+            <div className="game-scoreboard">
+                Scoreboard
+            </div>
+            }
+        </div>  
     );
 }
