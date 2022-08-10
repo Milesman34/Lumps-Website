f795d943b42369103861cfeb4d848eb9bd1c3b31
milesman34 <milesmanmusic@yahoo.com>
2022-08-10 13:10:11 -0400

    No scroll in start screen

--- src/components/pages/game/subpages/configs/GameConfigs.jsx
+++ src/components/pages/game/subpages/configs/GameConfigs.jsx
@@ -1,8 +1,10 @@
 import "./GameConfigs.css"
 
+import "../../../../../common.css"
+
 export default () => {
     return (
-        <div className="game-configs">
+        <div className="game-configs flex-center-column">
             Game Configs
         </div>
     );
