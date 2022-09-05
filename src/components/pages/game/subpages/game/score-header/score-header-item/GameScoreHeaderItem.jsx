295a4094b40a44543bfc8ca7e0998e519659ac53
milesman34 <milesmanmusic@yahoo.com>
2022-09-05 16:11:04 -0400

    Proper line breaks for longer names

--- src/components/pages/game/subpages/game/score-header/score-header-item/GameScoreHeaderItem.jsx
+++ src/components/pages/game/subpages/game/score-header/score-header-item/GameScoreHeaderItem.jsx
@@ -14,7 +14,7 @@ export default ({ score, name, minimum, maximum }) => {
     return (
         <div className="game-score-header-item flex-center-column">
             <div className="game-score-header-id-text flex-center-column">{name}</div>
-            <div className={"game-score-header-score-text flex-center-column" + extraClass}>{pluralized(score, "point")}</div>
+            <div className={"game-score-header-score-text flex-center-column " + extraClass}>{pluralized(score, "point")}</div>
         </div>
     );
 }
